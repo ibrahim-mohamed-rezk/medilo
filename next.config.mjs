4ee9f2098d2ccf032fd41708c5f7a0e4dc157b76
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-04-09 13:15:37 +0200

    b

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['gold-goose-774583.hostingersite.com'],
+    domains: ["gold-goose-774583.hostingersite.com"],
   },
+  output: "export",
 };
 
 export default nextConfig;
