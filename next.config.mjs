4ea760afa0c15d84808789bfa2df117407dfdd1e
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-04-09 13:53:28 +0200

    solve next vertion

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,18 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "gold-goose-774583.hostingersite.com",
-        port: "",
-        pathname: "/uploads/**",
-        search: "",
-      },
-      {
-        protocol: "https",
-        hostname: "gold-goose-774583.hostingersite.com",
-        port: "",
-        pathname: "/assets/**",
-        search: "",
-      },
-    ],
-  },
+    images: {
+        domains: ['gold-goose-774583.hostingersite.com'],
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'gold-goose-774583.hostingersite.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'gold-goose-774583.hostingersite.com',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
